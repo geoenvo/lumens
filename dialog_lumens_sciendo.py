0f55b8301f4a7d2aaa361be8df4555db9d5d3121
Seno A. K <seno@geo.co.id>
2015-12-14 15:18:57 +0700

    update dialog SCIENDO

--- dialog_lumens_sciendo.py
+++ dialog_lumens_sciendo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-import os, logging
+import os, logging, datetime
 ##from qgis.core import *
 ##from processing.tools import *
 from PyQt4 import QtCore, QtGui
@@ -27,47 +27,131 @@ class DialogLumensSCIENDO(QtGui.QDialog):
         self.dialogLayout = QtGui.QVBoxLayout()
         self.tabWidget = QtGui.QTabWidget()
         
-        self.tabSetup = QtGui.QWidget()
+        self.tabLandUseChangeModeling = QtGui.QWidget()
         self.tabResult = QtGui.QWidget()
         self.tabReport = QtGui.QWidget()
         self.tabLog = QtGui.QWidget()
         
-        self.tabWidget.addTab(self.tabSetup, 'Setup')
+        self.tabWidget.addTab(self.tabLandUseChangeModeling, 'Land Use Change Modeling')
         self.tabWidget.addTab(self.tabResult, 'Result')
         self.tabWidget.addTab(self.tabReport, 'Report')
         self.tabWidget.addTab(self.tabLog, 'Log')
         
-        self.layoutTabSetup = QtGui.QVBoxLayout()
+        self.layoutTabLandUseChangeModeling = QtGui.QVBoxLayout()
         self.layoutTabResult = QtGui.QVBoxLayout()
         self.layoutTabReport = QtGui.QVBoxLayout()
         self.layoutTabLog = QtGui.QVBoxLayout()
         
+        self.tabLandUseChangeModeling.setLayout(self.layoutTabLandUseChangeModeling)
+        self.tabResult.setLayout(self.layoutTabResult)
+        self.tabReport.setLayout(self.layoutTabReport)
+        self.tabLog.setLayout(self.layoutTabLog)
+        
         self.dialogLayout.addWidget(self.tabWidget)
         
         #***********************************************************
-        # Setup 'Setup' tab
+        # Setup 'Land Use Change Modeling' tab
         #***********************************************************
-        # 'Base year' GroupBox
-        self.groupBoxBaseYear = QtGui.QGroupBox('Base year')
-        self.layoutGroupBoxBaseYear = QtGui.QVBoxLayout()
-        self.groupBoxBaseYear.setLayout(self.layoutGroupBoxBaseYear)
+        # 'Functions' GroupBox
+        self.groupBoxLandUseChangeModelingFunctions = QtGui.QGroupBox('Functions')
+        self.layoutGroupBoxLandUseChangeModelingFunctions = QtGui.QVBoxLayout()
+        self.layoutGroupBoxLandUseChangeModelingFunctions.setAlignment(QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.groupBoxLandUseChangeModelingFunctions.setLayout(self.layoutGroupBoxLandUseChangeModelingFunctions)
+        self.layoutLandUseChangeModelingFunctionsInfo = QtGui.QVBoxLayout()
+        self.layoutLandUseChangeModelingFunctions = QtGui.QGridLayout()
+        self.layoutGroupBoxLandUseChangeModelingFunctions.addLayout(self.layoutLandUseChangeModelingFunctionsInfo)
+        self.layoutGroupBoxLandUseChangeModelingFunctions.addLayout(self.layoutLandUseChangeModelingFunctions)
+        
+        self.labelLandUseChangeModelingFunctionsInfo = QtGui.QLabel()
+        self.labelLandUseChangeModelingFunctionsInfo.setText('Lorem ipsum dolor sit amet...\n')
+        self.layoutLandUseChangeModelingFunctionsInfo.addWidget(self.labelLandUseChangeModelingFunctionsInfo)
+        
+        self.checkBoxCalculateTransitionMatrix = QtGui.QCheckBox('Calculate transition matrix')
+        self.checkBoxCreateRasterCubeOfFactors = QtGui.QCheckBox('Create raster cuve of factors')
+        self.checkBoxCalculateWeightOfEvidence = QtGui.QCheckBox('Calculate weight of evidence')
+        self.checkBoxSimulateLandUseChange = QtGui.QCheckBox('Simulate land use change')
+        self.checkBoxSimulateWithScenario = QtGui.QCheckBox('Simulate with scenario')
+        
+        self.layoutLandUseChangeModelingFunctions.addWidget(self.checkBoxCalculateTransitionMatrix)
+        self.layoutLandUseChangeModelingFunctions.addWidget(self.checkBoxCreateRasterCubeOfFactors)
+        self.layoutLandUseChangeModelingFunctions.addWidget(self.checkBoxCalculateWeightOfEvidence)
+        self.layoutLandUseChangeModelingFunctions.addWidget(self.checkBoxSimulateLandUseChange)
+        self.layoutLandUseChangeModelingFunctions.addWidget(self.checkBoxSimulateWithScenario)
+        
+        # 'Parameters' GroupBox
+        self.groupBoxLandUseChangeModelingParameters = QtGui.QGroupBox('Parameters')
+        self.layoutGroupBoxLandUseChangeModelingParameters = QtGui.QVBoxLayout()
+        self.layoutGroupBoxLandUseChangeModelingParameters.setAlignment(QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.groupBoxLandUseChangeModelingParameters.setLayout(self.layoutGroupBoxLandUseChangeModelingParameters)
+        self.layoutLandUseChangeModelingParametersInfo = QtGui.QVBoxLayout()
+        self.layoutLandUseChangeModelingParameters = QtGui.QGridLayout()
+        self.layoutGroupBoxLandUseChangeModelingParameters.addLayout(self.layoutLandUseChangeModelingParametersInfo)
+        self.layoutGroupBoxLandUseChangeModelingParameters.addLayout(self.layoutLandUseChangeModelingParameters)
+        
+        self.labelLandUseChangeModelingParametersInfo = QtGui.QLabel()
+        self.labelLandUseChangeModelingParametersInfo.setText('Lorem ipsum dolor sit amet...\n')
+        self.layoutLandUseChangeModelingParametersInfo.addWidget(self.labelLandUseChangeModelingParametersInfo)
+        
+        self.labelLandUseChangeModelingFactorsDir = QtGui.QLabel()
+        self.labelLandUseChangeModelingFactorsDir.setText('Factors directory:')
+        self.layoutLandUseChangeModelingParameters.addWidget(self.labelLandUseChangeModelingFactorsDir, 0, 0)
+        
+        self.lineEditLandUseChangeModelingFactorsDir = QtGui.QLineEdit()
+        self.lineEditLandUseChangeModelingFactorsDir.setReadOnly(True)
+        self.layoutLandUseChangeModelingParameters.addWidget(self.lineEditLandUseChangeModelingFactorsDir, 0, 1)
+        
+        self.buttonSelectLandUseChangeModelingFactorsDir = QtGui.QPushButton()
+        self.buttonSelectLandUseChangeModelingFactorsDir.setText('&Browse')
+        self.layoutLandUseChangeModelingParameters.addWidget(self.buttonSelectLandUseChangeModelingFactorsDir, 0, 2)
+        
+        self.labelLandUseChangeModelingLandUseLookup = QtGui.QLabel()
+        self.labelLandUseChangeModelingLandUseLookup.setText('Land use lookup table:')
+        self.layoutLandUseChangeModelingParameters.addWidget(self.labelLandUseChangeModelingLandUseLookup, 1, 0)
+        
+        self.lineEditLandUseChangeModelingLandUseLookup = QtGui.QLineEdit()
+        self.lineEditLandUseChangeModelingLandUseLookup.setReadOnly(True)
+        self.layoutLandUseChangeModelingParameters.addWidget(self.lineEditLandUseChangeModelingLandUseLookup, 1, 1)
+        
+        self.buttonSelectLandUseChangeModelingLandUseLookup = QtGui.QPushButton()
+        self.buttonSelectLandUseChangeModelingLandUseLookup.setText('&Browse')
+        self.layoutLandUseChangeModelingParameters.addWidget(self.buttonSelectLandUseChangeModelingLandUseLookup, 1, 2)
+        
+        self.labelLandUseChangeModelingBaseYear = QtGui.QLabel()
+        self.labelLandUseChangeModelingBaseYear.setText('Base &year:')
+        self.layoutLandUseChangeModelingParameters.addWidget(self.labelLandUseChangeModelingBaseYear, 2, 0)
+        
+        self.spinBoxLandUseChangeModelingBaseYear = QtGui.QSpinBox()
+        self.spinBoxLandUseChangeModelingBaseYear.setRange(1, 9999)
+        td = datetime.date.today()
+        self.spinBoxLandUseChangeModelingBaseYear.setValue(td.year)
+        self.layoutLandUseChangeModelingParameters.addWidget(self.spinBoxLandUseChangeModelingBaseYear, 2, 1)
+        self.labelLandUseChangeModelingBaseYear.setBuddy(self.spinBoxLandUseChangeModelingBaseYear)
+        
+        self.labelLandUseChangeModelingLocation = QtGui.QLabel()
+        self.labelLandUseChangeModelingLocation.setText('Location:')
+        self.layoutLandUseChangeModelingParameters.addWidget(self.labelLandUseChangeModelingLocation, 3, 0)
+        
+        self.lineEditLandUseChangeModelingLocation = QtGui.QLineEdit()
+        self.lineEditLandUseChangeModelingLocation.setText('location')
+        self.layoutLandUseChangeModelingParameters.addWidget(self.lineEditLandUseChangeModelingLocation, 3, 1)
+        self.labelLandUseChangeModelingLocation.setBuddy(self.lineEditLandUseChangeModelingLocation)
         
         # Process tab button
-        self.layoutButtonSetup = QtGui.QHBoxLayout()
-        self.buttonProcessSetup = QtGui.QPushButton()
-        self.buttonProcessSetup.setText('&Process')
-        self.layoutButtonSetup.setAlignment(QtCore.Qt.AlignRight)
-        self.layoutButtonSetup.addWidget(self.buttonProcessSetup)
+        self.layoutButtonLandUseChangeModeling = QtGui.QHBoxLayout()
+        self.buttonProcessLandUseChangeModeling = QtGui.QPushButton()
+        self.buttonProcessLandUseChangeModeling.setText('&Process')
+        self.layoutButtonLandUseChangeModeling.setAlignment(QtCore.Qt.AlignRight)
+        self.layoutButtonLandUseChangeModeling.addWidget(self.buttonProcessLandUseChangeModeling)
         
-        self.layoutTabSetup.addWidget(self.groupBoxBaseYear)
-        self.layoutTabSetup.addLayout(self.layoutButtonSetup)
-        
-        self.tabSetup.setLayout(self.layoutTabSetup)
+        # Place the GroupBoxes
+        self.layoutTabLandUseChangeModeling.addWidget(self.groupBoxLandUseChangeModelingFunctions)
+        self.layoutTabLandUseChangeModeling.addWidget(self.groupBoxLandUseChangeModelingParameters)
+        self.layoutTabLandUseChangeModeling.addLayout(self.layoutButtonLandUseChangeModeling)
         
         #***********************************************************
         # Setup 'Result' tab
         #***********************************************************
-        self.tabResult.setLayout(self.layoutTabResult)
+        
         
         #***********************************************************
         # Setup 'Report' tab
--- test_dialog.py
+++ test_dialog.py
@@ -9,8 +9,8 @@ from PyQt4 import QtGui, QtCore
 ##from dialog_lumens_pur import DialogLumensPUR
 ##from dialog_lumens_pur_referenceclasses import DialogLumensPURReferenceClasses
 ##from dialog_lumens_ta_opportunitycost import DialogLumensTAOpportunityCost
-from dialog_lumens_ta_regionaleconomy import DialogLumensTARegionalEconomy
-##from dialog_lumens_sciendo import DialogLumensSCIENDO
+##from dialog_lumens_ta_regionaleconomy import DialogLumensTARegionalEconomy
+from dialog_lumens_sciendo import DialogLumensSCIENDO
 ##from dialog_lumens_ques import DialogLumensQUES
 
 def main():
@@ -20,8 +20,8 @@ def main():
     ##dialog = DialogLumensPUR(None)
     ##dialog = DialogLumensPURReferenceClasses(None)
     ##dialog = DialogLumensTAOpportunityCost(None)
-    dialog = DialogLumensTARegionalEconomy(None)
-    ##dialog = DialogLumensSCIENDO(None)
+    ##dialog = DialogLumensTARegionalEconomy(None)
+    dialog = DialogLumensSCIENDO(None)
     ##dialog = DialogLumensQUES(None)
     
     dialog.show()
