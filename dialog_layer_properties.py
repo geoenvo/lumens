4daa9deb05fcc3c0a594c172cf719cacc8800dba
Seno A. K <seno@geo.co.id>
2016-06-16 17:14:57 +0700

    in progress: categorized style form

--- dialog_layer_properties.py
+++ dialog_layer_properties.py
@@ -21,7 +21,7 @@ class DialogLayerProperties(QtGui.QDialog):
         self.layer = layer
         self.main = parent
         self.dialogTitle = 'LUMENS Layer Properties - ' + self.layer.name()
-        self.layerSymbolFillColor = self.labelColor = QtGui.QColor(0, 0, 0) # black
+        self.layerSymbolFillColor = self.styleCategorizedColor = self.labelColor = QtGui.QColor(0, 0, 0) # black
         
         if self.main.appSettings['debug']:
             print 'DEBUG: DialogLayerProperties init'
@@ -39,9 +39,15 @@ class DialogLayerProperties(QtGui.QDialog):
         
         self.loadLayerSettings()
         
+        self.comboBoxStyleType.currentIndexChanged.connect(self.handlerChangeStyleType)
+        self.handlerChangeStyleType(0)
+        self.buttonAddStyleCategorized.clicked.connect(self.handlerAddStyleCategorized)
+        self.buttonDeleteStyleCategorized.clicked.connect(self.handlerDeleteStyleCategorized)
+        self.buttonDeleteAllStyleCategorized.clicked.connect(self.handlerDeleteAllStyleCategorized)
         self.sliderLayerTransparency.sliderMoved.connect(self.handlerSliderLayerTransparencyMoved)
         self.spinBoxLayerTransparency.valueChanged.connect(self.handlerSpinBoxLayerTransparencyValueChanged)
-        self.buttonLayerSymbolFillColor.clicked.connect(self.handlerSelectLayerSymbolFillColor)
+        self.buttonLayerSymbolFillColor.clicked.connect(self.handlerSelectFillColor)
+        self.buttonStyleCategorizedFillColor.clicked.connect(self.handlerSelectFillColor)
         self.buttonLabelColor.clicked.connect(self.handlerSelectLabelColor)
         self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.reject)
@@ -59,36 +65,130 @@ class DialogLayerProperties(QtGui.QDialog):
         self.layoutGroupBoxLayerStyle = QtGui.QVBoxLayout()
         self.layoutGroupBoxLayerStyle.setAlignment(QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
         self.groupBoxLayerStyle.setLayout(self.layoutGroupBoxLayerStyle)
+        
         self.layoutLayerStyleInfo = QtGui.QVBoxLayout()
-        self.layoutLayerStyle = QtGui.QGridLayout()
+        self.layoutLayerStyle = QtGui.QVBoxLayout()
         self.layoutGroupBoxLayerStyle.addLayout(self.layoutLayerStyleInfo)
+        
+        styleTypes = ['Single', 'Categorized']
+        self.comboBoxStyleType = QtGui.QComboBox()
+        self.comboBoxStyleType.addItems(styleTypes)
+        
+        self.layoutGroupBoxLayerStyle.addWidget(self.comboBoxStyleType)
         self.layoutGroupBoxLayerStyle.addLayout(self.layoutLayerStyle)
+        
+        self.groupBoxLayerTransparency = QtGui.QGroupBox('Transparency')
+        self.layoutGroupBoxLayerTransparency = QtGui.QGridLayout()
+        self.groupBoxLayerTransparency.setLayout(self.layoutGroupBoxLayerTransparency)
+        
+        self.styleTabWidget = QtGui.QTabWidget()
+        self.styleTabWidget.setTabPosition(QtGui.QTabWidget.North)
+        
+        self.tabStyleSingle = QtGui.QWidget()
+        self.layoutGroupBoxStyleSingle = QtGui.QGridLayout()
+        self.layoutGroupBoxStyleSingle.setAlignment(QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.tabStyleSingle.setLayout(self.layoutGroupBoxStyleSingle)
+        
+        self.tabStyleCategorized = QtGui.QWidget()
+        self.layoutGroupBoxStyleCategorized = QtGui.QGridLayout()
+        self.layoutGroupBoxStyleCategorized.setAlignment(QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.tabStyleCategorized.setLayout(self.layoutGroupBoxStyleCategorized)
+        
+        self.styleTabWidget.addTab(self.tabStyleSingle, 'Single')
+        self.styleTabWidget.addTab(self.tabStyleCategorized, 'Categorized')
+        
+        self.layoutLayerStyle.addWidget(self.groupBoxLayerTransparency)
+        self.layoutLayerStyle.addWidget(self.styleTabWidget)
         
         self.labelLayerStyleInfo = QtGui.QLabel()
         self.labelLayerStyleInfo.setText('Lorem ipsum dolor sit amet...\n')
         self.layoutLayerStyleInfo.addWidget(self.labelLayerStyleInfo)
         
+        # Transparency groupbox widgets
         self.labelLayerTransparency = QtGui.QLabel()
         self.labelLayerTransparency.setText('Transparency:')
-        self.layoutLayerStyle.addWidget(self.labelLayerTransparency, 0, 0)
+        self.layoutGroupBoxLayerTransparency.addWidget(self.labelLayerTransparency, 0, 0)
         
         self.sliderLayerTransparency = QtGui.QSlider()
         self.sliderLayerTransparency.setRange(0, 100)
         self.sliderLayerTransparency.setOrientation(QtCore.Qt.Horizontal)
-        self.layoutLayerStyle.addWidget(self.sliderLayerTransparency, 0, 1)
+        self.layoutGroupBoxLayerTransparency.addWidget(self.sliderLayerTransparency, 0, 1)
         
         self.spinBoxLayerTransparency = QtGui.QSpinBox()
         self.spinBoxLayerTransparency.setRange(0, 100)
-        self.layoutLayerStyle.addWidget(self.spinBoxLayerTransparency, 0, 2)
+        self.layoutGroupBoxLayerTransparency.addWidget(self.spinBoxLayerTransparency, 0, 2)
         
+        # Single style groupbox widgets
         self.labelLayerSymbolFillColor = QtGui.QLabel()
         self.labelLayerSymbolFillColor.setText('Fill color:')
-        self.layoutLayerStyle.addWidget(self.labelLayerSymbolFillColor, 1, 0)
+        self.layoutGroupBoxStyleSingle.addWidget(self.labelLayerSymbolFillColor, 0, 0)
         
         self.buttonLayerSymbolFillColor = QtGui.QPushButton()
+        self.buttonLayerSymbolFillColor.setObjectName('buttonLayerSymbolFillColor')
         self.buttonLayerSymbolFillColor.setFixedWidth(50)
         self.buttonLayerSymbolFillColor.setStyleSheet('background-color: {0};'.format(self.layerSymbolFillColor.name()))
-        self.layoutLayerStyle.addWidget(self.buttonLayerSymbolFillColor, 1, 1)
+        self.layoutGroupBoxStyleSingle.addWidget(self.buttonLayerSymbolFillColor, 0, 1)
+        
+        # Categorized style groupbox widgets
+        self.labelStyleCategorizedAttribute = QtGui.QLabel()
+        self.labelStyleCategorizedAttribute.setText('Attribute:')
+        self.layoutGroupBoxStyleCategorized.addWidget(self.labelStyleCategorizedAttribute, 0, 0)
+        
+        self.comboBoxStyleCategorizedAttribute = QtGui.QComboBox()
+        self.comboBoxStyleCategorizedAttribute.setDisabled(True)
+        self.layoutGroupBoxStyleCategorized.addWidget(self.comboBoxStyleCategorizedAttribute, 0, 1)
+        
+        self.labelStyleCategorizedFillColor = QtGui.QLabel()
+        self.labelStyleCategorizedFillColor.setText('Fill color:')
+        self.layoutGroupBoxStyleCategorized.addWidget(self.labelStyleCategorizedFillColor, 1, 0)
+        
+        self.buttonStyleCategorizedFillColor = QtGui.QPushButton()
+        self.buttonStyleCategorizedFillColor.setObjectName('buttonStyleCategorizedFillColor')
+        self.buttonStyleCategorizedFillColor.setFixedWidth(50)
+        self.buttonStyleCategorizedFillColor.setStyleSheet('background-color: {0};'.format(self.layerSymbolFillColor.name()))
+        self.layoutGroupBoxStyleCategorized.addWidget(self.buttonStyleCategorizedFillColor, 1, 1)
+        
+        self.labelStyleCategorizedValue = QtGui.QLabel()
+        self.labelStyleCategorizedValue.setText('Value:')
+        self.layoutGroupBoxStyleCategorized.addWidget(self.labelStyleCategorizedValue, 2, 0)
+        
+        self.lineEditStyleCategorizedValue = QtGui.QLineEdit()
+        self.layoutGroupBoxStyleCategorized.addWidget(self.lineEditStyleCategorizedValue, 2, 1)
+        
+        self.labelStyleCategorizedLabel = QtGui.QLabel()
+        self.labelStyleCategorizedLabel.setText('Label:')
+        self.layoutGroupBoxStyleCategorized.addWidget(self.labelStyleCategorizedLabel, 3, 0)
+        
+        self.lineEditStyleCategorizedLabel = QtGui.QLineEdit()
+        self.layoutGroupBoxStyleCategorized.addWidget(self.lineEditStyleCategorizedLabel, 3, 1)
+        
+        self.layoutButtonStyleCategorized = QtGui.QHBoxLayout()
+        self.layoutButtonStyleCategorized.setAlignment(QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.layoutButtonStyleCategorized.setSpacing(10)
+        
+        self.buttonAddStyleCategorized = QtGui.QPushButton()
+        self.buttonAddStyleCategorized.setText('Add')
+        self.buttonAddStyleCategorized.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        self.layoutButtonStyleCategorized.addWidget(self.buttonAddStyleCategorized)
+        
+        self.buttonDeleteStyleCategorized = QtGui.QPushButton()
+        self.buttonDeleteStyleCategorized.setText('Delete')
+        self.buttonDeleteStyleCategorized.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        self.layoutButtonStyleCategorized.addWidget(self.buttonDeleteStyleCategorized)
+        
+        self.buttonDeleteAllStyleCategorized = QtGui.QPushButton()
+        self.buttonDeleteAllStyleCategorized.setText('Delete All')
+        self.buttonDeleteAllStyleCategorized.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        self.layoutButtonStyleCategorized.addWidget(self.buttonDeleteAllStyleCategorized)
+        
+        self.layoutGroupBoxStyleCategorized.addLayout(self.layoutButtonStyleCategorized, 4, 1)
+        
+        self.tableStyleCategorized = QtGui.QTableWidget()
+        headersTableStyleCategorized = ['Color', 'Value', 'Label']
+        self.tableStyleCategorized.setColumnCount(len(headersTableStyleCategorized))
+        self.tableStyleCategorized.setHorizontalHeaderLabels(headersTableStyleCategorized)
+        self.tableStyleCategorized.verticalHeader().setVisible(False)
+        self.layoutGroupBoxStyleCategorized.addWidget(self.tableStyleCategorized, 5, 0, 1, 2)
         
         ######################################################################
         
@@ -171,6 +271,10 @@ class DialogLayerProperties(QtGui.QDialog):
         self.comboBoxLayerAttribute.addItems(sorted(attributes))
         self.comboBoxLayerAttribute.setEnabled(True)
         
+        self.comboBoxStyleCategorizedAttribute.clear()
+        self.comboBoxStyleCategorizedAttribute.addItems(sorted(attributes))
+        self.comboBoxStyleCategorizedAttribute.setEnabled(True)
+        
         # Get layer transparency setting
         self.sliderLayerTransparency.setValue(self.layer.layerTransparency())
         self.spinBoxLayerTransparency.setValue(self.layer.layerTransparency())
@@ -178,7 +282,9 @@ class DialogLayerProperties(QtGui.QDialog):
         # Get layer symbol fill color
         symbols = self.layer.rendererV2().symbols()
         self.layerSymbolFillColor = symbols[0].color()
+        self.styleCategorizedColor = symbols[0].color()
         self.buttonLayerSymbolFillColor.setStyleSheet('background-color: {0};'.format(self.layerSymbolFillColor.name()))
+        self.buttonStyleCategorizedFillColor.setStyleSheet('background-color: {0};'.format(self.layerSymbolFillColor.name()))
         
         # Get layer label settings
         self.p = QgsPalLayerSettings()
@@ -213,14 +319,19 @@ class DialogLayerProperties(QtGui.QDialog):
         self.sliderLayerTransparency.setValue(newValue)
     
     
-    def handlerSelectLayerSymbolFillColor(self):
-        """Slot method when the layer color button is clicked.
+    def handlerSelectFillColor(self):
+        """Slot method when the fill color button is clicked.
         """
         dialog = QtGui.QColorDialog(self.layerSymbolFillColor)
         
         if dialog.exec_():
-            self.layerSymbolFillColor = dialog.selectedColor()
-            self.buttonLayerSymbolFillColor.setStyleSheet('background-color: {0};'.format(self.layerSymbolFillColor.name()))
+            selectedColor = dialog.selectedColor()
+            sender = self.sender()
+            if sender.objectName() == 'buttonLayerSymbolFillColor':
+                self.layerSymbolFillColor = selectedColor
+            elif sender.objectName == 'buttonStyleCategorizedFillColor':
+                self.styleCategorizedColor = selectedColor
+            sender.setStyleSheet('background-color: {0};'.format(selectedColor.name()))
     
     
     def handlerSelectLabelColor(self):
@@ -232,6 +343,64 @@ class DialogLayerProperties(QtGui.QDialog):
             self.labelColor = dialog.selectedColor()
             self.buttonLabelColor.setStyleSheet('background-color: {0};'.format(self.labelColor.name()))
     
+    
+    def handlerChangeStyleType(self, currentIndex):
+        """Slot method for selecting the style tab.
+        
+        Args:
+            currentIndex (int): the index number of the selected combobox item.
+        """
+        styleType = self.comboBoxStyleType.currentText()
+        if styleType == 'Single':
+            self.styleTabWidget.setCurrentWidget(self.tabStyleSingle)
+        elif styleType == 'Categorized':
+            self.styleTabWidget.setCurrentWidget(self.tabStyleCategorized)
+    
+    
+    def handlerAddStyleCategorized(self):
+        """Slot method for adding a categorized style.
+        """
+        value = QtGui.QTableWidgetItem(self.lineEditStyleCategorizedValue.text())
+        label = QtGui.QTableWidgetItem(self.lineEditStyleCategorizedLabel.text())
+        if value.text() == '' and label.text() == '':
+            return
+        currentRowCount = self.tableStyleCategorized.rowCount()
+        color = QtGui.QTableWidgetItem('')
+        color.setBackground(QtGui.QBrush(self.styleCategorizedColor))
+        color.setFlags(color.flags() & ~QtCore.Qt.ItemIsEditable)
+        value.setFlags(value.flags() & ~QtCore.Qt.ItemIsEditable)
+        label.setFlags(label.flags() & ~QtCore.Qt.ItemIsEditable)
+        self.tableStyleCategorized.insertRow(currentRowCount)
+        self.tableStyleCategorized.setItem(currentRowCount, 0, color)
+        self.tableStyleCategorized.setItem(currentRowCount, 1, value)
+        self.tableStyleCategorized.setItem(currentRowCount, 2, label)
+        self.lineEditStyleCategorizedValue.setText('')
+        self.lineEditStyleCategorizedLabel.setText('')
+    
+    
+    def handlerDeleteStyleCategorized(self):
+        """Slot method for deleting a categorized style.
+        """
+        currentRow = self.tableStyleCategorized.currentRow()
+        self.tableStyleCategorized.removeRow(currentRow)
+    
+    
+    def handlerDeleteAllStyleCategorized(self):
+        """Slot method for deleting all categorized styles.
+        """
+        reply = QtGui.QMessageBox.question(
+            self,
+            'Delete All',
+            'Do you want to delete all?',
+            QtGui.QMessageBox.Yes|QtGui.QMessageBox.Cancel,
+            QtGui.QMessageBox.Cancel
+        )
+        
+        if reply == QtGui.QMessageBox.Yes:
+            self.tableStyleCategorized.setRowCount(0)
+        elif reply == QtGui.QMessageBox.Cancel:
+            pass
+            
     
     #***********************************************************
     # Process dialog
